60deba195b1dbd255ba40a680c803298c83b6350
Igor Torquato <igor.torquato.20@gmail.com>
2025-08-09 18:06:12 -0300

    add: algoritmo selecao reta

--- listaRetas.h
+++ listaRetas.h
@@ -16,6 +16,8 @@ typedef struct {
 void criarListaRetas(ListaRetas* lista);
 void ListaRetasInserirFim(ListaRetas* lista, Reta r);
 void desenharRetas(ListaRetas* lista);
+int codigo(double x, double y, double xmin, double xmax, double ymin, double ymax);
+int pickReta(double x0, double y0, double x1, double y1, double mx, double my, double t);
 void selecionarRetaMaisProxima(ListaRetas* lista, double x, double y);
 void deletarRetasSelecionadas(ListaRetas* lista);
 
