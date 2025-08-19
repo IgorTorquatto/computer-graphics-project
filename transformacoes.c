eb4a92b31a4877429c4fbc1faf2c4b70fe103aec
Igor Torquato <igor.torquato.20@gmail.com>
2025-08-19 17:42:17 -0300

    add: rotação de ponto e colocando na main.c todos os eventos de teclado

--- transformacoes.c
+++ transformacoes.c
@@ -1,4 +1,5 @@
 #include "transformacoes.h"
+#include <math.h>
 
 // Função para multiplicar um ponto por uma matriz 3x3
 void multiplicarMatrizPonto(const double matriz[3][3], Ponto *ponto) {
@@ -26,3 +27,35 @@ void transladarPonto(Ponto *ponto, double deslocX, double deslocY) {
 
     multiplicarMatrizPonto(matrizTranslacao, ponto);
 }
+
+// Função que cria a matriz de rotação em torno de um centro
+void pegarMatrizRotacao(double matrizDestino[3][3], unsigned char tecla, Ponto centro) { //composta
+    // ângulo de 2 graus em radianos
+    double passoAngulo = 2 * (M_PI / 180.0);
+    double cosseno = cos(passoAngulo);
+    double seno = sin(passoAngulo);
+
+    // matriz de rotação composta com translação
+    double matrizRotacao[3][3] = {
+        {cosseno, -seno, ((-cosseno * centro.x) + (seno * centro.y) + centro.x)},
+        {seno,  cosseno, ((-seno * centro.x) - (cosseno * centro.y) + centro.y)},
+        {0.0,    0.0,    1.0}
+    };
+
+    // copiando para matrizDestino
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrizDestino[i][j] = matrizRotacao[i][j];
+        }
+    }
+}
+
+// Função que aplica a matriz em um ponto
+void aplicarMatrizNoPonto(double matriz[3][3], Ponto* p) {
+    double xOriginal = p->x;
+    double yOriginal = p->y;
+
+    p->x = matriz[0][0] * xOriginal + matriz[0][1] * yOriginal + matriz[0][2];
+    p->y = matriz[1][0] * xOriginal + matriz[1][1] * yOriginal + matriz[1][2];
+}
+
